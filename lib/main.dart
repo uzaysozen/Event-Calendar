a8e85a9e8dec3c30de877bba3ee597b5f3fc6cfa
uzays <uzaysozen@gmail.com>
2022-08-06 00:54:16 +0300

    Added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:countdown_app/screens/event_list.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +11,13 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: EventList(),
+      home: AnimatedSplashScreen(
+          duration: 2000,
+          splash: Icon(Icons.calendar_month_outlined, color: Colors.white, size: 140,),
+          nextScreen: EventList(),
+          splashTransition: SplashTransition.fadeTransition,
+          pageTransitionType: PageTransitionType.fade,
+          backgroundColor: Colors.blueAccent),
       debugShowCheckedModeBanner: false,
     );
   }
